98bfb7f81c8e4cc73248180d96d1ffa3ec12a869
Julio Ugalde <julio.ugalde404@gmail.com>
2024-09-23 10:46:18 -0600

    Script were updated

--- api-rest-nodejs/services/mailer.js
+++ api-rest-nodejs/services/mailer.js
@@ -1,81 +1,83 @@
-'use strict'
+"use strict";
 
-const fs = require('fs');
-const handlebars = require('handlebars');
-const ejs = require('ejs');
-const nodemailer = require('nodemailer');
-const smtpTransport = require('nodemailer-smtp-transport');
-const path = require('path');
+const fs = require("fs");
+const handlebars = require("handlebars");
+const ejs = require("ejs");
+const nodemailer = require("nodemailer");
+const smtpTransport = require("nodemailer-smtp-transport");
+const path = require("path");
 
 // Models
-const Sale = require('../models/sale');
-const SaleDetail = require('../models/saleDetail');
+const Sale = require("../models/sale");
+const SaleDetail = require("../models/saleDetail");
 
 //giicmgaatntckzds
 
-const initMailer = async function(saleId){
-    const readHTMLFile = function(path, callback){
-        fs.readFile(path, { encoding: 'utf-8'}, function(err, html){
-            if(err){
-                throw err;
-                callback(err);
-            }else{
-                callback(null, html);
-            }
-        })
-    }
-    
-    const transporter = nodemailer.createTransport(smtpTransport({
-        service: 'gmail',
-        host: 'smtp.gamil.com',
-        auth:{
-            user: 'headshotmusic404@gmail.com',
-            pass: 'giicmgaatntckzds'
-        }
-    }));
+const initMailer = async function (saleId) {
+  const readHTMLFile = function (path, callback) {
+    fs.readFile(path, { encoding: "utf-8" }, function (err, html) {
+      if (err) {
+        throw err;
+        callback(err);
+      } else {
+        callback(null, html);
+      }
+    });
+  };
 
-    // Get customer data
-    const sale = await Sale.findById({_id:saleId}).populate('customer');
-    const saleDetail = await SaleDetail.find({sale: saleId}).populate('product');
-
-
-    const data = {
-        customerNames: sale.customer.names + ' ' + sale.customer.surnames,
-        saleDate: new Date(sale.createdAt),
-        subtotal: sale.total,
-        customerEmail: sale.customer.email,
-        saleDetail: saleDetail
-    }
-
-
-    console.log('---------------');
-    console.log(data);
-
-    // Send sale detail to html template
-    readHTMLFile(process.cwd() + '/mail-template.html', (err, html) =>{
-        let rest_html = ejs.render(html, { data: data });
-    
-        var template = handlebars.compile(rest_html);
-        var htmlToSend = template({ op:true })
-    
-        var mailOptions = {
-            from: 'headshotmusic404@gmail.com',
-            to: 'subl1metype@gmail.com',
-            subject: 'A shop...',
-            html: htmlToSend
-        };
-
-        // res.status(200).send({ data: true });
-        transporter.sendMail(mailOptions, function(err, info) {
-            if(!err){
-                console.log('Email sent successfully to: ' + sale.customer.email);
-            }
-
-            console.log(process.cwd());
-        })
+  const transporter = nodemailer.createTransport(
+    smtpTransport({
+      service: "gmail",
+      host: "smtp.gamil.com",
+      auth: {
+        user: "headshotmusic404@gmail.com",
+        pass: "giicmgaatntckzds",
+      },
     })
-}
+  );
+
+  // Get customer data
+  const sale = await Sale.findById({ _id: saleId }).populate("customer");
+  const saleDetail = await SaleDetail.find({ sale: saleId }).populate(
+    "product"
+  );
+
+  const data = {
+    customerNames: sale.customer.names + " " + sale.customer.surnames,
+    saleDate: new Date(sale.createdAt),
+    subtotal: sale.total,
+    customerEmail: sale.customer.email,
+    saleDetail: saleDetail,
+  };
+
+  console.log("---------------");
+  console.log(data);
+
+  // Send sale detail to html template
+  readHTMLFile(process.cwd() + "/mail-template.html", (err, html) => {
+    let rest_html = ejs.render(html, { data: data });
+
+    var template = handlebars.compile(rest_html);
+    var htmlToSend = template({ op: true });
+
+    var mailOptions = {
+      from: "headshotmusic404@gmail.com",
+      to: "subl1metype@gmail.com",
+      subject: "A shop...",
+      html: htmlToSend,
+    };
+
+    // res.status(200).send({ data: true });
+    transporter.sendMail(mailOptions, function (err, info) {
+      if (!err) {
+        console.log("Email sent successfully to: " + sale.customer.email);
+      }
+
+      console.log(process.cwd());
+    });
+  });
+};
 
 module.exports = {
-    initMailer,
-}
+  initMailer,
+};
--- api-rest-nodejs/services/stripe.js
+++ api-rest-nodejs/services/stripe.js
@@ -1,43 +1,43 @@
-'use strict'
+"use strict";
 
-const stripe = require('stripe')('sk_test_51L1yDyIX0ejDpXz2NQusVQUCrRwIQQiBlib2JuDIAchzIqGUSOlChzTx2dQAmORzqOyLcWfhLY2NGOn1aCmkCqiE005e7zcbhU');
-
+const stripe = require("stripe")(
+  "sk_test_51L1yDyIX0ejDpXz2NQusVQUCrRwIQQiBlib2JuDIAchzIqGUSOlChzTx2dQAmORzqOyLcWfhLY2NGOn1aCmkCqiE005e7zcbhU"
+);
 
 // Generate payment intent
-const generatePaymentIntent = async function(data){
+const generatePaymentIntent = async function (data) {
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: parseFloat(data.amount) * 100,
+    currency: "usd",
+    payment_method_types: ["card"],
+    payment_method: data.payment_method,
+    description: `Prueba de pago para -> ${data.user}`,
+    receipt_email: "headshotmusic404@gmail.com",
+  });
 
-    const paymentIntent = await stripe.paymentIntents.create({
-        amount: parseFloat(data.amount) * 100,
-        currency: 'usd',
-        payment_method_types: ['card'],
-        payment_method: data.payment_method,
-        description: `Prueba de pago para -> ${data.user}`,
-        receipt_email: 'headshotmusic404@gmail.com'
-    })                                                              
+  console.log("ok");
+  return paymentIntent;
+};
 
-    console.log('ok');
-    return paymentIntent;       
+const generatePaymentMethod = async function (token) {
+  const paymentMethod = await stripe.paymentMethods.create({
+    type: "card",
+    card: { token },
+  });
 
-}
+  return paymentMethod;
+};
 
-const generatePaymentMethod = async function(token){
+const confirmPaymentIntent = async function (id, token) {
+  const paymentIntent = await stripe.paymentIntents.confirm(id, {
+    payment_method: token,
+  });
 
-    const paymentMethod = await stripe.paymentMethods.create({
-        type: 'card',
-        card: { token },
-    })
-
-    return paymentMethod;
-}
-
-const confirmPaymentIntent = async function(id, token){
-    const paymentIntent = await stripe.paymentIntents.confirm(id, { payment_method: token} );
-
-    return paymentIntent;
-}
+  return paymentIntent;
+};
 
 module.exports = {
-    generatePaymentIntent,
-    generatePaymentMethod,
-    confirmPaymentIntent
-}
+  generatePaymentIntent,
+  generatePaymentMethod,
+  confirmPaymentIntent,
+};
