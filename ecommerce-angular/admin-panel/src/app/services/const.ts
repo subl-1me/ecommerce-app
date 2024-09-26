ec2dfa1a044495b43a6a23c5229a4c95adb87fdd
Julio Ugalde <julio.ugalde404@gmail.com>
2024-09-25 19:37:31 -0600

    API url const changed

--- ecommerce-angular/admin-panel/src/app/services/const.ts
+++ ecommerce-angular/admin-panel/src/app/services/const.ts
@@ -1,6 +1,6 @@
 import { HttpHeaders } from '@angular/common/http';
 
 export var constans = {
-    url: 'https://api-rest-ecommerce-production.up.railway.app/api/',
-    headers: new HttpHeaders().set('Content-Type', 'application/json'),
-}
+  url: 'https://ecommerce-app-1-l8py.onrender.com/api/',
+  headers: new HttpHeaders().set('Content-Type', 'application/json'),
+};
