cb0f29a94680df6a663e257c1b8a41ae2744ebb4
subl-1me <julio.ugalde404@gmail.com>
2024-12-08 10:17:05 -0700

    Quick fixes with API URL

--- api-rest-nodejs/controllers/adminController.js
+++ api-rest-nodejs/controllers/adminController.js
@@ -1,81 +1,80 @@
-'use strict'
+"use strict";
 
-var Admin = require('../models/admin');
-var bcrypt = require('bcrypt-nodejs');
-var jwt = require('../helpers/jwt');
+var Admin = require("../models/admin");
+var bcrypt = require("bcrypt-nodejs");
+var jwt = require("../helpers/jwt");
 
-const adminRegister = async function(req, res){
-    //
-    var data = req.body;
-    var adminArray = [];
+const adminRegister = async function (req, res) {
+  //
+  var data = req.body;
+  var adminArray = [];
 
-    // Check if email is already taken
-    adminArray = await Admin.find({
-        email: data.email
-    });
+  // Check if email is already taken
+  adminArray = await Admin.find({
+    email: data.email,
+  });
 
-    if(adminArray.length == 0){
-        // check await
-        // register
-        if(data.password){
-            bcrypt.hash(data.password, null, null, async function(err, hash){
-                if(hash){
-                    data.password = hash;
-                    var reg = await Admin.create(data);
-                    res.status(200).send({
-                        message: 'User created sucessfully',
-                        user: reg
-                    })
-                }else{
-                    res.status(200).send({message: 'Error bcrypting password'})
-                }
-            });
-        }else{
-            res.status(200).send({
-                message: 'There is not a pssword'
-            })
+  if (adminArray.length == 0) {
+    // check await
+    // register
+    if (data.password) {
+      bcrypt.hash(data.password, null, null, async function (err, hash) {
+        if (hash) {
+          data.password = hash;
+          var reg = await Admin.create(data);
+          res.status(200).send({
+            message: "User created sucessfully",
+            user: reg,
+          });
+        } else {
+          res.status(200).send({ message: "Error bcrypting password" });
         }
-    }else{
+      });
+    } else {
+      res.status(200).send({
+        message: "There is not a pssword",
+      });
+    }
+  } else {
+    res.status(200).send({
+      message: "Email already taken. Try with another",
+    });
+  }
+};
+
+const adminLogin = async function (req, res) {
+  var data = req.body;
+  var adminArray = [];
+
+  adminArray = await Admin.find({ email: data.email });
+
+  if (adminArray.length == 0) {
+    res.status(200).send({ message: "User does not exists." });
+  } else {
+    // login
+    let user = adminArray[0];
+
+    console.log(user);
+
+    bcrypt.compare(data.password, user.password, async function (error, check) {
+      if (error) {
+        res.status(200).send({ message: "Something went wrong." });
+      }
+
+      if (check) {
         res.status(200).send({
-            message: 'Email already taken. Try with another'
-        })
-    }
-}
-
-const adminLogin = async function(req, res){
-    var data = req.body;
-    var adminArray = [];
-
-    adminArray = await Admin.find({ email: data.email });
-
-    if(adminArray.length == 0){
-        res.status(200).send({ message: 'User does not exists.' })
-    }else{
-        // login
-        let user = adminArray[0];
-
-        console.log(user);
-
-        bcrypt.compare(data.password, user.password, async function(error, check){
-
-            if(error){
-                res.status(200).send({ message: 'Something went wrong.' })
-            }
-
-            if(check){
-                res.status(200).send({
-                    user: user,
-                    token: jwt.createToken(user),
-                    message: 'Login successfully.'
-                });
-            }else{
-                res.status(200).send({ message: 'Password does not match.' })
-            }
+          user: user,
+          token: jwt.createToken(user),
+          message: "Login successfully.",
         });
-    }
-}
+      } else {
+        res.status(200).send({ message: "Password does not match." });
+      }
+    });
+  }
+};
 
 module.exports = {
-    adminRegister,
-    adminLogin
-}
+  adminRegister,
+  adminLogin,
+};
--- ecommerce-angular/admin-panel/src/app/services/const.ts
+++ ecommerce-angular/admin-panel/src/app/services/const.ts
@@ -1,7 +1,7 @@
 import { HttpHeaders } from '@angular/common/http';
 
 export var constans = {
-  url: 'http://localhost:4201/api/',
-  productLinkHref: 'http://localhost:4200/',
+  url: process.env || 'http://localhost:4201/api/',
+  productLinkHref: process.env || 'http://localhost:4200/',
   headers: new HttpHeaders().set('Content-Type', 'application/json'),
 };
