ad1c7ed46fa1d6f45d343ad82bb95fc60cf0d435
subl-1me <julio.ugalde404@gmail.com>
2024-12-08 10:26:17 -0700

    Quick fix API ENV url statement

--- ecommerce-angular/admin-panel/src/app/services/const.ts
+++ ecommerce-angular/admin-panel/src/app/services/const.ts
@@ -1,7 +1,7 @@
 import { HttpHeaders } from '@angular/common/http';
 
 export var constans = {
-  url: process.env || 'http://localhost:4201/api/',
-  productLinkHref: process.env || 'http://localhost:4200/',
+  url: process.env['API_URL'] || 'http://localhost:4201/api/',
+  productLinkHref: process.env['productLinkHRef'] || 'http://localhost:4200/',
   headers: new HttpHeaders().set('Content-Type', 'application/json'),
 };
