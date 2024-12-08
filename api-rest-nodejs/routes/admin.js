6aaf7279339515fd5aad41110232fcd424aef129
subl-1me <julio.ugalde404@gmail.com>
2024-12-08 12:06:53 -0700

    Quick fix environment variables

--- api-rest-nodejs/routes/admin.js
+++ api-rest-nodejs/routes/admin.js
@@ -1,12 +1,11 @@
-'use strict'
+"use strict";
 
-var express = require('express');
-var adminController = require('../controllers/adminController');
+var express = require("express");
+var adminController = require("../controllers/adminController");
 
 var routes = express.Router();
 
-routes.post('/register_admin', adminController.adminRegister);
-routes.post('/login_admin', adminController.adminLogin);
+routes.post("/register_admin", adminController.adminRegister);
+routes.post("/login_admin", adminController.adminLogin);
 
 module.exports = routes;
-
