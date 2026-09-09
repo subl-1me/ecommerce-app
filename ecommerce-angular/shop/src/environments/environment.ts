f6436a7267a486b68db0e9219ffe8f0003ecb31a
subl-1me <julio.ugalde404@gmail.com>
2026-09-08 21:44:55 -0600

    Changed environment to local

--- ecommerce-angular/shop/src/environments/environment.ts
+++ ecommerce-angular/shop/src/environments/environment.ts
@@ -2,5 +2,6 @@ export const environment = {
   production: true,
   stripe_pk:
     'pk_test_51L1yDyIX0ejDpXz2xriFFkc2ov5XmwysFD5u0BAJJbtt5RLUnxKJY5X0QWVW4gqYxaukYilaFyA8OBPFMtNE2KfS005oWRzBCH',
-  API_URL: 'https://ecommerce-app-1-l8py.onrender.com/api/',
+  API_URL: 'http://localhost:3000/api/',
+  API_URL_LOCAL: 'http://localhost:3000/api/',
 };
