45c5ffc9560bb2529d5984e35f07cc88c1d33093
subl-1me <julio.ugalde404@gmail.com>
2026-09-08 19:17:11 -0600

    Edit Customer View ~ Updated view & service (provisional)

--- api-rest-nodejs/helpers/safeCredentials.js
+++ api-rest-nodejs/helpers/safeCredentials.js
@@ -1,16 +1,20 @@
-'use strict'
+"use strict";
 
 // filter password
-exports.safeCredentials = function(user){
-    var credentials = {
-        sub: user._id,
-        names: user.names,
-        surnames: user.surnames,
-        profile: user.profile,
-        phone: user.phone,
-        email: user.email,
-        dni: user.dni | undefined
-    }
+exports.safeCredentials = function (user) {
+  var credentials = {
+    sub: user._id,
+    names: user.names,
+    surnames: user.surnames,
+    profile: user.profile,
+    gender: user.gender,
+    phone: user.phone,
+    email: user.email,
+    dni: user.dni | undefined,
+    notes: user.notes,
+    createdAt: user.createdAt,
+    updatedAt: user.updatedAt,
+  };
 
-    return credentials;
-}
+  return credentials;
+};
